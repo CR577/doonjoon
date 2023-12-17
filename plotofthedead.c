2ac08075f160972a8d573ca40c1c484d4d206ed3
CR577 <wyattsmee577@gmail.com>
2023-12-17 14:08:22 -0800

    initial commit

---
+++ plotofthedead.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <unistd.h>  // sleep() uses seconds, not ms, and in an unsigned int (why??), but usleep() uses... MICROSECONDS?????
+
+int main() {
+    FILE *file;
+    printf("PLOT OF THE DEAD\nCR577/CREATING REALITIES STUDIOS MMXXIII\n");
+    sleep(2);
+    system("clear");
+    file = fopen("title.txt", "r");
+    char title[600];
+    while(fgets(title, 600, file)) {
+        usleep(400000);
+        printf("%s", title);
+        fflush(stdout);
+    }
+    fclose(file);
+    sleep(2);
+    return 0;
+}
